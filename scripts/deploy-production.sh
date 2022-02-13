e709a94d3249898252417ece60acd2bf52c07484
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2022-02-13 16:32:21 -0500

    Precompile added, but need yarn and node

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,5 +2,8 @@
 echo "Installing gems"
 bundle install
 
+echo "Precompile assets"
+bundle exec bin/rails assets:precompile
+
 echo "DEPLOYING TO PRODUCTION!"
 bundle exec bin/rails s --binding=0.0.0.0 --port=80
